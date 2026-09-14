c3eeed8e72cb9e777a8923f19d56bc76f4a52cae
seonimk <seonimk012@gmail.com>
2026-09-14 15:23:26 +0900

    first commit

---
+++ challenge/week1/first.cpp
@@ -0,0 +1,8 @@
+// 첫 번째 예제 프로그램
+#include <iostream>                     // 헤더파일
+using namespace std;                    // 이름 공간 설정
+
+int main() {                            // 함수 선언
+    cout << "Hello, World!" << endl;    // 화면에 문자열 출력
+    return 0;
+}
